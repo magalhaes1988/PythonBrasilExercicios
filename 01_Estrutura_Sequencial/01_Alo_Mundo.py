831c19487ee716fdb33a7ba6f1e589a6a2f08b6e
magalhaes1988 <magalhaes1988@gmail.com>
2022-06-03 15:04:17 -0300

    master

---
+++ 01_Estrutura_Sequencial/01_Alo_Mundo.py
@@ -0,0 +1,4 @@
+#Pyton 3.10.4
+#Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+
+print('Alo mundo')
---
+++ 01_Estrutura_Sequencial/02_Imprime_Numero.py
@@ -0,0 +1,3 @@
+num = int(input("Digite um número inteiro: "))
+
+print(f'O número informado foi {num}.')
